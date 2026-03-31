0e516db3ac1e6ec1e438b7abb4e5d489f4baf37e
kashishswaroop04 <kswaroop@charlotte.edu>
2026-03-30 22:32:02 -0400

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -14,31 +14,59 @@ export default function Footer() {
         </a>
       </p>
 
-      <a href="https://kashishswaroop04.github.io/itis3135/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://kashishswaroop04.github.io/itis3135/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         ITIS3135
       </a>{' '}
       |{' '}
-      <a href="https://your-vercel-url-here.vercel.app/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://itis3135-react-seven-pi.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         ITIS3135@Vercel
       </a>{' '}
       |{' '}
-      <a href="https://github.com/kashishswaroop04" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/kashishswaroop04"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         GitHub
       </a>{' '}
       |{' '}
-      <a href="https://webpages.charlotte.edu/801295137" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://webpages.charlotte.edu/801295137"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         CLT Web
       </a>{' '}
       |{' '}
-      <a href="https://kashishswaroop04.github.io" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://kashishswaroop04.github.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         GitHub.io
       </a>{' '}
       |{' '}
-      <a href="https://www.freecodecamp.org/kashish_swaroop" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.freecodecamp.org/kashish_swaroop"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         FreeCodeCamp
       </a>{' '}
       |{' '}
-      <a href="https://www.linkedin.com/in/kashish-swaroop-9173741b7/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/kashish-swaroop-9173741b7/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         LinkedIn
       </a>
 
